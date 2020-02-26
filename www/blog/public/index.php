0e3eb80b541393784db6fde09efc245abe047c06
孟德辉 <1559664966@qq.com>
2020-02-26 12:09:20 +0800

    up

--- www/blog/public/index.php
+++ www/blog/public/index.php
@@ -3,17 +3,17 @@ echo '<p>php环境搭建成功</p>';
 
 
 try {
-    new PDO('mysql:host=mysql;dbname=mysql', 'root', '123456');//host地址直接使用link指定的标签mysql
+    new PDO("mysql:host=172.19.0.6;dbname=mysql", 'root', '123456');//host地址直接使用link指定的标签mysql
     echo '<p>mysql连接成功</p>';
 } catch (Exception $e) {
     echo '<p>mysql连接失败 : '.$e->getMessage().'</p>';
 } finally {
     try {
         $mem = new Memcached();
-        $memcached=$mem->addServer('memcached',11211);
+        $memcached=$mem->addServer('172.19.0.4',11211);
 
         $red = new Redis();
-        $redis = $red->connect('redis', 6379);//地址直接使用link指定的标签redis
+        $redis = $red->connect('172.19.0.3', 6379);//地址直接使用link指定的标签redis
 
         if($memcached != true && !$redis ){
             echo "redis和memcached都连接失败";
